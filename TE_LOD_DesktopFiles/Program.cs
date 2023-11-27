a5fec1625a978c9dd924d11b78420c8713914c17
Christopher <krzysiek-smaga@outlook.com>
2023-11-27 01:21:47 +0100

    Dodaj pliki projektów.

---
+++ TE_LOD_DesktopFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿/*   
+ *      Dokumentacja pomocna do naszej dzisiejszej lekcji:
+ *      https://learn.microsoft.com/pl-pl/dotnet/api/system.io?view=net-7.0
+ */
+
+using System;
+using System.IO;    // Wymagana przestrzeń nazwy, by móc operować plikami
+
+namespace TE_LOD_DesktopFiles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Obejmij kod obsługą błędów
+
+
+        }
+    }
+}
+
+/* Pomoc do zajęć:
+ *      - Klasa File, Directory, Path
+ *      - Operacje na plikach (czy istnieje, odczyt, zapis, przenieś, usuń)
+ *      - Operacje na katalogach (czy istnieje, stwórz, usuń, lista wszystkich plików oraz katalogów)
+ *      - Operacje na strumieniach (StreamReader, StreamWriter)
+ */
