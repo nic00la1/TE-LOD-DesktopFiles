ccc3bcec4c572bff4e6363411dd308babd4d819b
Nicola Kaleta <nic00la.kaleta@gmail.com>
2023-12-04 15:53:56 +0100

    siema robie zadanie

--- TE_LOD_DesktopFiles/Program.cs
+++ TE_LOD_DesktopFiles/Program.cs
@@ -15,19 +15,82 @@ namespace TE_LOD_DesktopFiles
             // Obejmij kod obsługą błędów
             try
             {
-                string path = "plik.txt";
-                // Sprawdzenie czy plik istnieje
-                if(File.Exists(path))
+                Console.WriteLine("Podaj ścieżkę do sprawdzenia: ");
+               /* string path = Console.ReadLine();
+
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
                 {
-                // Wyświetlenie całej zawartości tekstu
-                Console.WriteLine(File.ReadAllText(path));
+                    Console.WriteLine(dir);
+                }
+               */ 
+
+                /* ============================================
+                Sprawdzenie czy plik istnieje
+                if (File.Exists(path))
+                {
+                    // Wyświetlenie całej zawartości tekstu
+                    Console.WriteLine(File.ReadAllText(path));
                 }
                 else
                 {
-                    Console.WriteLine("Nie ma tego pliku");
-                    File.Create(path); // Tworzenie nowego pliku
+                    Console.WriteLine("Podaj zawartość do tego pliku");
+
+                    // Zapisuje tresc uzytkownika do pliku
+                    string data = Console.ReadLine();
+                    File.WriteAllText(path, data);
                 }
-            } catch (IOException ex)
+                */
+
+                /* ============================================
+                List<String> files = new List<String>();
+                foreach (string dir in Directory.GetDirectories(path)) {
+                    // Dodawanie do listy nazwy katalogu
+                    files.Add(Path.GetDirectoryName(dir));
+                }
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    files.Add(Path.GetFileName(file));
+                }
+                foreach (string element in files)
+                {
+                    // sprawdzenie czy plik ma rozszerzenie .txt
+                    if(Path.GetExtension(element) == ".txt") {
+                        Console.WriteLine("TEKST");
+                    }
+                    Console.WriteLine(element);
+                } =============================================== */
+
+                /* =======================================================
+                Console.WriteLine("Jaki plik chcesz skopiowac?");
+                string file = Console.ReadLine();
+                Console.WriteLine("Do jakiego katalogu chcesz przeniesc ");
+                string new_file = Console.ReadLine();
+
+                string copied = Path.Combine(new_file, Path.GetFileName(file));
+                // skopiuj plik
+
+
+                // Przenoszenie pliku
+                // File.Move(source, destination)
+                File.Copy(file, copied);
+                
+                if (File.Exists(copied)) {
+                    Console.WriteLine("skopiowano");
+                } else
+                {
+                    Console.WriteLine("nie skopiowano");
+                }
+                ====================================================== */
+                string path = "tekst.txt";
+                StreamReader reader = new StreamReader(path);
+                string data = "";
+                while ((data += reader.ReadLine()) != null) { 
+                    Console.WriteLine(data);
+                } 
+
+            }
+            catch (IOException ex)
             {
                 Console.WriteLine(ex.Message); // Napisz wiadomosc, gdy nie ma takiego pliku
             }
